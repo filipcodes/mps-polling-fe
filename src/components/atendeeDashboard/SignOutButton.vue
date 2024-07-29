7437664e9a7c749ba79abb866db7c77b4b97b327
SIPOS Filip SK79A <filip.sipos@student.leaf.academy>
2024-07-30 00:35:49 +0200

    adding localiyation to the signOutButton

--- src/components/atendeeDashboard/SignOutButton.vue
+++ src/components/atendeeDashboard/SignOutButton.vue
@@ -3,11 +3,10 @@ import AppButtonLink from '@/components/AppButtonLink.vue'
 </script>
 
 <template>
-  <AppButtonLink type="tertiary" @click="handleSignOut"
-    >Odhlásiť sa</AppButtonLink
-  >
+  <AppButtonLink type="tertiary" @click="handleSignOut">{{
+    $t('general.logOut')
+  }}</AppButtonLink>
 </template>
-
 <script>
 import { getAuth, signOut } from 'firebase/auth'
 
