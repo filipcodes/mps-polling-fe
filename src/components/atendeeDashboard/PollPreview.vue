f071b5fcdb6feba1b417e61fd68a3af4edc563b9
SIPOS Filip SK79A <filip.sipos@student.leaf.academy>
2025-02-03 18:47:36 +0100

    extract PollPreviewVoteButton from PollPreview for code clarity

--- src/components/atendeeDashboard/PollPreview.vue
+++ src/components/atendeeDashboard/PollPreview.vue
@@ -1,19 +1,16 @@
 <script setup>
 import AppButtonLink from '@/components/AppButtonLink.vue'
-import { get } from 'firebase/database'
+import PollPreviewVoteButton from './PollPreviewVoteButton.vue'
 import { user } from '@/states/userState.js'
 
 import {
-  getFirestore,
   collection,
-  getDocs,
-  addDoc,
-  query,
-  where,
-  onSnapshot,
   doc,
-  updateDoc,
-  setDoc
+  getDocs,
+  getFirestore,
+  query,
+  setDoc,
+  where
 } from 'firebase/firestore'
 
 import Cookies from 'js-cookie'
@@ -30,26 +27,12 @@ import Cookies from 'js-cookie'
       >
         hlasovanie {{ poll.number }}: <span>{{ poll.name }}</span>
       </h3>
-      <AppButtonLink
-        :type="selectedOption && isActive ? 'primary' : 'inactive'"
-        class="w-full sm:w-fit"
+
+      <PollPreviewVoteButton
+        :selectedOption="selectedOption"
+        :isActive="isActive"
         @click="handleVote"
-      >
-        <div class="flex items-center gap-3 justify-center bevan">
-          {{ isActive ? 'Hlasovať' : 'Odhlasované' }}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            class="h-8 w-8"
-          >
-            <title>Hlasovať</title>
-            <path
-              class=""
-              d="M18,13H17.32L15.32,15H17.23L19,17H5L6.78,15H8.83L6.83,13H6L3,16V20A2,2 0 0,0 5,22H19A2,2 0 0,0 21,20V16L18,13M17,7.95L12.05,12.9L8.5,9.36L13.46,4.41L17,7.95M12.76,2.29L6.39,8.66C6,9.05 6,9.68 6.39,10.07L11.34,15C11.73,15.41 12.36,15.41 12.75,15L19.11,8.66C19.5,8.27 19.5,7.64 19.11,7.25L14.16,2.3C13.78,1.9 13.15,1.9 12.76,2.29Z"
-            />
-          </svg>
-        </div>
-      </AppButtonLink>
+      />
     </div>
     <div class="grid sm:grid-cols-3 grid-cols-2 sm:gap-4 gap-2 w-full">
       <AppButtonLink
@@ -74,7 +57,7 @@ export default {
   data() {
     return {
       selectedOption: '',
-      isActive: true
+      isActive: false
     }
   },
   props: {
@@ -87,13 +70,7 @@ export default {
   async mounted() {
     // this should  check whether it is active. currently, it uses cookies, which is not the best practice
 
-    const storedValue = Cookies.get('IsActive')
-    if (storedValue) {
-      this.isActive = storedValue === 'true'
-    }
-
     // See whether the user has voted in the relevant poll by fetching firestore:
-    console.log('Poll ID: ', this.poll.id)
     const db = getFirestore()
     const pollRef = collection(db, this.poll.id)
     const q = query(pollRef, where('userId', '==', user.data.uid))
@@ -127,11 +104,7 @@ export default {
     }
   },
 
-  unmounted() {
-    Cookies.remove('IsActive')
-  },
-
-  components: [AppButtonLink]
+  components: [AppButtonLink, PollPreviewVoteButton]
 }
 </script>
 
